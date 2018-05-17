7d930d5a9c0d3c0eda8074ff53a0e3299479ac1a
Allumik <33635216+Allumik@users.noreply.github.com>
2018-05-17 10:58:14 +0300

    Add files via upload

---
+++ Lammas.java
@@ -0,0 +1,33 @@
+package application;
+
+public class Lammas {
+	
+	public int x;
+	public int y;
+	public int s;
+	
+	public Lammas(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getS() {
+		return s;
+	}
+	public void setS(int s) {
+		this.s = s;
+	}
+
+}
